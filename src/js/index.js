558b7d209828e6bbdce2a868cd37672c4820b745
Денис Пелевин <denispelevin@MacBook-Pro-Denis.local>
2021-03-01 17:34:40 +0300

    update form

--- src/js/index.js
+++ src/js/index.js
@@ -1,60 +1,31 @@
 "use strict";
-import Swal from "sweetalert2";
+import "./form.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const createHtmlForEmail = (value) => {
-    return `<div>
-      <div>
-        email: <b>${value}</b>
-      </div>
-    </div>`;
-  };
-  const createLetterData = (value) => ({
-    to: "wrisycle@42.works",
-    subject: "wrisycle contact form",
-    text: "yo",
-    html: createHtmlForEmail(value),
-  });
+  const toggleEl = document.querySelector('.toggle') 
+  const toggleVariants = toggleEl.querySelectorAll('.toggle__variant')
+  const formCheckbox = document.querySelector('.form__toggle-checkbox')
+  const businessNameEl = document.querySelector('.form__business-name')
 
-  let sending = false;
-
-  formElem.onsubmit = (e) => {
-    e.preventDefault();
-    if (!sending) {
-      const formData = new FormData(formElem);
-      let letterData;
-      for (var [key, value] of formData.entries()) {
-        letterData = JSON.stringify(createLetterData(value));
-      }
-      sending = true;
-      fetch("https://api.42.works/mailer", {
-        method: "POST",
-        body: letterData,
-        headers: {
-          "Content-Type": "application/json",
-        },
+  toggleVariants.forEach((el) => {
+    el.addEventListener('click', (event) => {
+      toggleVariants.forEach(el => {
+        el.classList.remove('active')
       })
-        .then((res) => {
-          sending = false;
-          if (res.status === 200) {
-            Swal.fire("Thanks!", "Message sent!", "success");
-          } else {
-            Swal.fire({
-              icon: "error",
-              title: "Oops...",
-              text: "Something went wrong!",
-            });
-          }
-        })
-        .catch((err) => {
-          console.error("err", err);
-          sending = false;
-          Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "Something went wrong!",
-          });
-        });
-    }
-  };
+
+      const currentVariant = event.currentTarget
+      currentVariant.classList.add('active')
+      const variantValue = currentVariant.dataset.value
+      if (variantValue === 'business') {
+        businessNameEl.classList.remove('hidden')
+        formCheckbox.checked = true
+        businessNameEl.required = true
+      } else {
+        businessNameEl.classList.add('hidden')
+        formCheckbox.checked = false
+        businessNameEl.required = false
+      }
+    })
+  })
+
 });
